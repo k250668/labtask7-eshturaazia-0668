3e62d29464a5f0982ffe5cdf259c21fa000e2770
Esha <k250668@nu.edu.pk>
2025-10-16 13:13:40 +0500

    Update Untitled4.cpp

--- Untitled4.cpp
+++ Untitled4.cpp
@@ -3,19 +3,22 @@ int main() {
 	int array[10];
 	int i;
 	int f;
-
+int freq[100] = {0};
 	for (i = 0;i<10;i++) {
 			printf("Enter your input: ");
 	scanf("%d", &array[i]); 
 		}
-	 for ( i =0; i < 10 ; i++ ) {
-	 if (array[i] == array[i+1]) {
-	 	array[i+1] = -1;
-	 }
-
-	 }
-	 for ( i = 0;i<10;i++){
-	 	printf("%d", array[i]);
-	 }
+		for ( i =0;i<10;i++) {
+			if ( freq[array[i]] == 0) {
+			freq[array[i]] = 1;
+			}
+			else {
+				array[i] = -1;
+			}
+		}
+	printf("Updated list") ;
+	for ( i =0 ; i<10;i++) {
+		printf("%d ", array[i]);
+	}
 	return 0;
 }
