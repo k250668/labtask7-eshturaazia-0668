178fb47b49741d03e1e1c1e95203d2620e5544d6
Esha <k250668@nu.edu.pk>
2025-10-22 12:50:13 +0300

    Update labtask7 q3.c

--- labtask7 q3.c
+++ labtask7 q3.c
@@ -1,41 +1,44 @@
-#include <stdio.h> 
-int main() {int p=0;
-	int f=0;
-	int pass[p];
-	int fail[f];
-	int i;
-	int mark;
-	int passum = 0;
-	int failsum = 0;
-	for ( i =0 ; i<10 ; i++){
-		printf("Enter marks (0-10): ");
-		scanf("%d",&mark);
-		if (mark > 5 && mark<10) {
-			pass[p] = mark;
-			p++;
-			passum = passum + mark;
-		}
-		else if (mark <= 5 && mark>=0) {
-			fail[f] = mark;
-			f++;
-		     failsum = failsum + mark;
-		}
-	}
-	printf("Pass marks are: ");
-	for ( i = 0 ; i<p;i++) {
-		printf("%d", pass[i]);
-	}
-	int passaverage = 0;
-	passaverage = passum/p;
+#include <stdio.h>
 
-	printf("Average of pass marks is : %d ", passaverage);
-	printf("fail marks: \n");
-	for ( i = 0 ; i<f;i++) {
-			printf("%d", fail[i]);
-	}
-		int failaverage = 0;
-	failaverage = failsum/f;
-		printf("Average of fail marks is : %d  ", failaverage);
+int main() {
+    int pass[10], fail[10];
+    int p = 0, f = 0;
+    int i, mark;
+    int passsum = 0, failsum = 0;
+	float passavg = 0;
+	float failavg = 0;
+
+    for (i = 0; i < 10; i++) {
+        printf("Enter marks (0-10): ");
+        scanf("%d", &mark);
+
+        if (mark >= 6 && mark <= 10) {
+            pass[p] = mark;
+            p++;
+            passsum += mark;
+        } 
+        else if (mark >= 0 && mark <= 5) {  fail[f] = mark;
+            f++;
+            failsum += mark;
+        } 
+    }
+	printf("\nPass marks are: ");
+    for (i = 0; i < p; i++) {
+        printf("%d ", pass[i]);
+    }
+
+   passavg = (float)passsum / p;
+	 printf("\nAverage of pass marks: %.2f\n", passavg);
+
+    printf("\nFail marks are: ");
+    for (i = 0; i < f; i++) {
+        printf("%d ", fail[i]);
+    }
+	failavg = (float)failsum / f; 
+	printf("\nAverage of fail marks: %.2f\n", failavg);
+	
+
 	
 return 0;
 }
+ 
